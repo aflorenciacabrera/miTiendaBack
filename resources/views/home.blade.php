7c27a392c5400ce7037396c4c9aa022570bc6704
Florencia <aflorenciacabrera@gmail.com>
2020-04-30 09:21:48 -0300

    prue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+                            
                         </div>
                     @endif
                     @if(Auth::user()->hasRole('admin'))
