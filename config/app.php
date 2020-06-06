99646419d2032172c420239b897a3b9f32157fb3
Florencia <aflorenciacabrera@gmail.com>
2020-06-05 23:13:22 -0300

    borrado de domPdf

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\JwtAuthServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+       
         
 
     ],
@@ -229,7 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'JwtAuth' => App\Helpers\JwtAuth::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        
        
     ],
 
